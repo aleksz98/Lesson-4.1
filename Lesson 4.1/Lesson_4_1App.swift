8cac178b26931a61cc0ec5f38fb8c10b29636fdf
Aleksz <aleksznoob@gmail.com>
2023-06-08 23:49:03 +0200

    Initial Commit

---
+++ Lesson 4.1/Lesson_4_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_4_1App.swift
+//  Lesson 4.1
+//
+//  Created by Иван on 08.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_4_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
